8c986b379ba55f0c607073cb6cb5f16f060b5bad
ana.cesantos10 <ana.cesantos10@L130682476W10-1>
2024-03-01 22:06:12 -0300

    teste

---
+++ CALCULADORA_GORJETA/index.js
@@ -0,0 +1,49 @@
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+const { parse } = require('path')
+
+inicio()
+function inicio(){
+    bill()
+    function bill(){
+    inquirer
+        .prompt([{
+            name: 'amount',
+            message: 'Digite o valor total da conta',
+            type: "number"
+        }])
+        .then((answer)=>{
+            const amount = answer['amount']
+
+            if(amount <= 0){
+                console.log(chalk.bgRed.black('Este número de conta não é valido!'))
+                return bill()
+            }
+            return numPessoas()
+        })
+    }
+
+    function numPessoas(){
+        inquirer
+        .prompt([{
+            name: 'people',
+            message: 'Insira a quantidade de pessoas para pagar a conta',
+            type: "number"
+        }])
+        .then((answer) =>{
+            const people = answer['people']
+            if(people <= 0){
+                console.info('Este número de pessoas não é valido!')
+                return numPessoas()
+            }
+            return valorTotal()
+        })
+    }
+ function valorTotal(amount, people){
+   gorjeta = amount + (amount * 0.10);
+ valordividido = (amount + gorjeta) / parseInt(people);
+  console.log(`Cada pessoa tem que pagar: ${valordividido} ${gorjeta} ${amount} ${people}`)
+
+   }
+}
+    
